d3213d0c971cbee8e88b1f30f8df2f8d69e570e8
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-07-03 14:46:18 +0530

    style: added google fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      fontFamily: {
+        roboto: ['var(--font-roboto)'],
+        mono: ['var(--font-jetbrains-mono)', 'monospace'],
+      },
     },
   },
   plugins: [],
