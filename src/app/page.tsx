27543237c2a7568f5a3a483d9aeb1dccb823ce0d
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-07-04 09:49:52 +0530

    chore: added side bar and depend components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
 }
