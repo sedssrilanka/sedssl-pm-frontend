99b0a44fe235da787047fa7f51d9b92a5d964999
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-07-04 10:08:12 +0530

    chore: added sidebar items

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,20 +1,62 @@
+import { AlarmClockCheck, Lightbulb, NotebookPen, ScrollText } from 'lucide-react';
+
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from '@/components/ui/sidebar';
+
+// Menu items.
+const items = [
+  {
+    title: 'Proposals',
+    url: '#',
+    icon: Lightbulb,
+  },
+  {
+    title: 'Projects',
+    url: '#',
+    icon: NotebookPen,
+  },
+  {
+    title: 'My Tasks',
+    url: '#',
+    icon: AlarmClockCheck,
+  },
+  {
+    title: 'Reports',
+    url: '#',
+    icon: ScrollText,
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
   );
 }
