62fd05e281ce3ea60fa5110594d444d05334b82a
Thawshi Srikanth <thawshi.dev@protonmail.com>
2025-07-06 12:26:35 +0530

    fix: toggle state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${roboto.variable} ${jetbrainsMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
